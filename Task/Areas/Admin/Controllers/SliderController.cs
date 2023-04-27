72329630233fefebdabd95cbe1b047e40f8f37db
Sahinshf <shahinrsha@code.edu.az>
2023-04-27 17:40:06 +0400

    Task is added

---
+++ Task/Areas/Admin/Controllers/SliderController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Task.Contexts;
+
+namespace Task.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SliderController : Controller
+    {
+
+        private readonly AppDbContext _context;
+
+        public SliderController(AppDbContext context)
+        {
+            _context = context;       
+        }
+
+        public IActionResult Index()
+        {
+
+            List<Slide> slides = _context.Slides.ToList();
+
+            return View(slides);
+        }
+
+        public IActionResult Create() {
+        
+            return View();  
+        }
+    }
+}
---
+++ Task/ViewModel/HomeViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Task.Models;
+
+namespace Task.ViewModel
+{
+    public class HomeViewModel
+    {
+        public List<Slide> Slides { get; set; }
+        public List<Service> Services { get; set; }     
+    }
+}
